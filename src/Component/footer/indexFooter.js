bdb9a0ba5c05d71649d2718cedfad8af8e637718
yosuanovry <yosuanovry@gmail.com>
2023-05-16 11:02:56 +0700

    fix responsive landing page

--- src/Component/footer/indexFooter.js
+++ src/Component/footer/indexFooter.js
@@ -1,20 +1,21 @@
 import React from "react";
+import style from './footer.module.css'
 
 export default function Footer() {
   return (
-    <div className="container-fluid d-flex justify-content-center align-items-center" style={{ minHeight: "450px", backgroundColor: "#efc81a", marginTop:'100px' }}>
-      <div className="row d-flex flex-column" style={{marginBottom:'-90px'}}>
-        <div className="col d-flex justify-content-center h1" style={{ fontSize: "72px", color:'#2E266F'}}>
+    <div className={`container-fluid d-flex justify-content-center align-items-center ${style.footContainer}`} >
+      <div className="row d-flex flex-column">
+        <div className={`col d-flex justify-content-center ${style.footHeaderText}`}>
           Eat, Cook, Repeat
         </div>
-        <div className="col d-flex justify-content-center mb-5" style={{ fontSize: "24px" }}>
+        <div className={`col d-flex justify-content-center mb-5 ${style.footDescText}`}>
           Share Your Best Recipe By Uploading Here !
         </div>
         <div className="col d-flex justify-content-center align-items-end">
-          <div className="row" style={{}}>
+          <div className="row-sm">
             <div className="col-1 mt-5"></div>
             <div className="col-10" style={{margin:'20px'}}>
-              <div className="d-flex align-items-end justify-content-start mt-4" style={{color:'#707070'}}>
+              <div className="d-flex align-items-end justify-content-start" style={{color:'#707070'}}>
                 <div className="p-3">Product</div>
                 <div className="p-3">Company</div>
                 <div className="p-3" style={{maxWidth:'200x'}}>Learn More</div>
--- src/Pages/Home/indexHome.js
+++ src/Pages/Home/indexHome.js
@@ -11,72 +11,64 @@ import indisalad from '../../pictures/indisalad.png'
 import sugsalm from '../../pictures/sugsalm.png'
 import toast from '../../pictures/toast.png'
 import {Link} from 'react-router-dom'
-
+import style from './home.module.css';
 
 
 export default function Home() {
   
   return (
     <>
-    <div>
+    <div className={style.body}>
         <NavbarProfile />
         
         <div className="container-fluid mt-5">
-        <div className="row">
+        <div className={`row`}>
             <div className="col-lg-6 d-flex justify-content-center align-items-center">
                 <div className="row d-flex flex-column justify-content-center">
                     <div className="col">
-                    <h1 className="text-header" style={{color: '#2e266f', fontWeight:'600', fontSize:'60px'}}>Discover Recipe</h1>
+                    <h1 className={style.textHeader}>Discover Recipe</h1>
                     </div>
                     <div className="col">
-                        <h1 className="text-header" style={{color: '#2e266f', fontWeight:'600', fontSize:'60px'}}>& Delicious Food</h1>
+                        <h1 className={style.textHeader}>& Delicious Food</h1>
                         </div>
-                        <div className="col">
+                        <div className={`col ${style.searchBar}`}>
                 <Link to='/search'>
-                    <input type="text" className="form-control" id="search" aria-describedby="emailHelp" placeholder="Search, Restaurant, Food"  style={{}}/>
+                    <input type="text" className={`form-control`}id="search" aria-describedby="emailHelp" placeholder="Search, Restaurant, Food"/>
                   </Link>
                         </div>
                   </div>
             </div>
-            <div className="gradient-1 col-lg-6 d-flex justify-content-center align-items-center">
-                <img src={eggs} alt="" className="rounded float-end me-5" style={{width: '600px'}} />
+            <div className={`col-lg-6 d-flex justify-content-center align-items-center mt-4 ${style.eggImage}`}>
+                <img src={eggs} alt="" className={`rounded float-end ${style.eggImage}`} />
             </div>
     </div>
 </div>
 
 
-<div className="container-fluid">
-    <div className="row">
-        <div className="col-6"></div>
-        <div className="gradient-2 col-6 d-flex justify-content-end align-items-center" style={{height: '300px'}}>
-        </div>
-    </div>
-</div>
 
-
-<div className="container-fluid">
-    <div className="row">
+<div className="container-fluid mt-5">
+    <div className={`row`}>
         <div className="col-1"></div>
-        <div className="col-6 d-flex align-items-center border-start border-5 border-warning" style={{height: '100px', margin:'20px', borderWidth:'2vh'}}>
-            <h2 className="title-2 ps-2" style={{fontWeight:'600'}}>Popular For You !</h2>
+        <div className={`col-6 d-flex align-items-center border-start border-5 border-warning ${style.textHeader1}`}>
+            <h2 className="ps-2" style={{fontWeight:'600'}}>Popular For You !</h2>
         </div>
     </div>
 </div>
 
 <div className="container-fluid">
-    <div className="row">
-        <div className="col-6 d-flex align-items-center justify-content-center" style={{height: '100vh'}}>
-            <img src={toast} alt="" className="d-flex align-items-center" style={{maxWidth: '80vh'}}/>
+    <div className={`row-sm ${style.row1}`}>
+        <div className={`col-6 d-flex align-items-center justify-content-center ${style.imageCrash}`}>
+            <img src={toast} alt="" className={`d-flex align-items-center ${style.toastImage}`}/>
         </div>
-        <div className="col-6 d-flex align-items-center">
-            <div className="row d-flex flex-column align-items-center justify-content-center" style={{margin: '20px'}}>
-                <div className="col-8">
-                    <h1 style={{lineHeight: '7.5vh'}}>Healthy Bone Broth Ramen (Quick & Easy)</h1>
+        <div className={`col-6-sm d-flex align-items-center ${style.imageRow}`}>
+            <div className={`row-sm d-flex flex-column align-items-center justify-content-center ${style.textMargin}`}>
+                <div className="col-8-sm">
+                    <p className={style.imageTextHeader}>Healthy Bone Broth Ramen (Quick & Easy)</p>
                 </div>
-                <div className="col-8 mt-3" style={{fontSize: '1.3vw' ,fontWeight:'500'}}>
+                <div className={`col-8-sm ${style.imageTextDesc}`} >
                     <p>Quick + Easy Chicken Bone Broth Ramen- Healthy chicken ramen in a hurry? That’s right!</p>
                 </div>
-                <div className="col-8">
+                <div className="col-8-sm">
                     <button className="btn btn-warning text-white shadow-none mt-3" style={{}}>Learn More</button>
                 </div>
             </div>
@@ -84,7 +76,7 @@ export default function Home() {
     </div>
 </div>
 
-<div className="container-fluid pt-5 mt-5">
+<div className={`container-fluid`}>
     <div className="row mt-5">
         <div className="col-1"></div>
         <div className="col-6 d-flex align-items-center border-start border-5 border-warning" style={{height: '100px',borderWidth: '2vh'}}>
@@ -94,20 +86,20 @@ export default function Home() {
 </div>
 
 
-<div className="container-fluid" style={{}}>
-    <div className="row">
-        <div className="col-6">
-            <img src={burger} alt="" className="rounded mx-auto d-block mt-5" style={{maxWidth: '80vh'}} />
+<div className="container-fluid">
+    <div className={`row-sm ${style.row1}`}>
+        <div className={`col-6 d-flex align-items-center justify-content-center ${style.imageCrash}`}>
+            <img src={burger} alt="" className={`rounded mx-auto d-block mt-5 ${style.burgerImage}`} />
         </div>
-        <div className="col-6 d-flex align-items-center">
-            <div className="row d-flex flex-column align-items-center justify-content-center" style={{}}>
-                <div className="col-8">
-                    <h1 style={{lineHeight: '6vh'}}>Healthy Bone Broth Ramen (Quick & Easy)</h1>
+        <div className={`col-6-sm d-flex align-items-center ${style.imageRow}`}>
+            <div className={`row-sm d-flex flex-column align-items-center justify-content-center ${style.textMargin}`}>
+                <div className="col-8-sm">
+                    <p className={style.imageTextHeader}>Healthy Bone Broth Ramen (Quick & Easy)</p>
                 </div>
-                <div className="col-8 mt-3" style={{fontSize: '1.3vw' , fontWeight:'500'}}>
+                <div className={`col-8-sm ${style.imageTextDesc}`}>
                     <p>Quick + Easy Chicken Bone Broth Ramen- Healthy chicken ramen in a hurry? That’s right!</p>
                 </div>
-                <div className="col-8">
+                <div className="col-8-sm">
                     <button className="btn btn-warning text-white shadow-none mt-3" style={{}}>Learn More</button>
                 </div>
             </div>
@@ -116,7 +108,7 @@ export default function Home() {
 </div>
 
 
-<div className="container-fluid pt-5 mt-5">
+<div className={`container-fluid mb-5`}>
     <div className="row mt-5">
         <div className="col-1"></div>
         <div className="col-6 d-flex align-items-center border-start border-5 border-warning" style={{height: '100px', borderWidth: '2vh'}}>
@@ -125,47 +117,40 @@ export default function Home() {
     </div>
 </div>
 
-<div className="container-fluid mt-5">
-    <div className="row d-flex justify-content-evenly">
+<div className={`container-fluid ${style.cardContainer}`}>
+    <div className={`row d-flex justify-content-evenly`}>
         <div className="col-3">
-            <img src={chickare} alt="" className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fontSize: '1.5vw', width: '130px'}}>Chicken Kare</h5>
+            <img src={chickare} alt="" className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Chicken Kare</h5>
         </div>
         <div className="col-3">
-            <img src={bombch} alt="" className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fonSize: '1.5vw', width: '130px'}}>Bomb Chicken</h5>
+            <img src={bombch} alt="" className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Bomb Chicken</h5>
         </div>
         <div className="col-3">
-            <img src={bsmooth} alt="" className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fontSize: '1.5vw', width: '220px'}}>Banana Smoothie Pop</h5>
+            <img src={bsmooth} alt="" className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Banana Smoothie Pop</h5>
         </div>
     </div>
 </div>
 
 <div className="container-fluid mt-5">
-    <div className="row d-flex justify-content-evenly">
+    <div className={`row d-flex justify-content-evenly`}>
         <div className="col-3">
-            <img src={coflava} alt=""className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fontSize: '1.5vw', width: '190px'}}>Coffee Lava Cake</h5>
+            <img src={coflava} alt=""className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Coffee Lava Cake</h5>
         </div>
         <div className="col-3">
-            <img src={sugsalm} alt="" className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fontSize: '1.5vw', width: '130px'}}>Sugar Salmon</h5>
+            <img src={sugsalm} alt="" className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Sugar Salmon</h5>
         </div>
         <div className="col-3">
-            <img src={indisalad} alt="" className="rounded" style={{maxWidth: '25vw'}}/>
-            <h5 style={{fontSize: '1.5vw' , width: '120px'}}>Indian Salad</h5>
+            <img src={indisalad} alt="" className={`rounded ${style.cardImage}`}/>
+            <h5 className={style.cardImageText}>Indian Salad</h5>
         </div>
     </div>
 </div>
 
-<div className="container-fluid pt-5 mt-5">
-    <div className="row">
-        <div className="col-6" style={{height: '15vh'}}></div>
-        <div className="col-6 d-flex justify-content-end align-items-center">
-        </div>
-    </div>
-</div>
     </div>
 
     <Footer />
